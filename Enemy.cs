2e9dc7e629700da059740c3dd5860bedaaa5c4b6
mirumeow <139230934+mirumeow@users.noreply.github.com>
2024-03-31 23:49:58 +0900

    Add files via upload
    enemy수정- 턴종료 버튼으로 턴교체, enemy 공격/방어 데미지 랜덤

--- Enemy.cs
+++ Enemy.cs
@@ -1,113 +1,152 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*이후 enemycomponent에 추가할 스크립트*/
+/*각 enemy마다 추가될 스크립트임.
+1. 적은 unitcode로 enemyStats에서 설정, public 변수로 외부에서 지정 가능
+unitcode 수정 시 enemy변경이 가능함.
+2. enemy는 턴 시작시 랜덤으로 공격 혹은 방어(현재 공격:방어 7:3 확률)
+3. 행동력(스피드)개념: 스피드가 더 높으면 배틀 시작시 선제공격
+4. 현재 player: enemy 1:1전투로 구현됨. 이후 추가 가능
+5. 턴제-GM에서 턴 관리하는 대신 player의 턴종료button으로 enemy턴 시작*/
+/*수정해야할것
+1. enemy 방어량도 공격량과 동일하게 했는데(랜덤도 동일) 이후 수정해야할지
+2. player 턴 종료 후 카드를 받아오는 방식(class? struct?)->script맡으신 분과 조율
+3. player 스크립트 맡으신 분도 턴종료 시 호출되는 함수가 있을텐데 통일시켜야함
+------------------개인------------------
+4. 애니메이션(+FSM)추가
+5. endbattle시 씬전환*/
 public enum State{
     awake, playerTurn, enemyTurn, dead
-}
+}//player와 enemy 턴 구별, enemy 죽음 상태를 판별할 enum
 
 public class Enemy : MonoBehaviour
 {
-    public Status status;
-    public State state;
-    public unitCode unitcode;
-    public int random;//공격/방어(6:4) 랜덤으로 선택시 사용
-
-    public int playerSpeed;//행동력:상대의 1.8배 이상일경우 한번 더 행동(아직 구현x),상대보다 클 경우 시작시 선제공격
-    private bool hadTurn=false;//턴 중복체크용 변수
-    public int playerAtk;//각 턴마다 받아오는 player 공격량
+    public enemyStats status;//적 스테이터스 클래스 객체변수
+    public State state;//현재 턴 상태
+    private int random;//공격:방어=7:3 매턴마다 난수
+    public unitCode unitcode;//enemy 구별 코드-> 유니티에서 변경가능하도록 public.
     
-    public int playerHP;
-    /*player변수들-이후 player 구현 클래스에서 변수 가져올 예정*/
+    /*player변수들-매턴마다 선택하는 카드의 요소와 player의 상태를 대체할 변수
+    매턴마다 복수의 카드가 선택됨->enemy에게 들어갈 데미지량의 총합*/
+    public int playerSpeed=2;//행동력:상대보다 클 경우 시작시 선제공격
     
-
-    public Status setEnemy(unitCode unitcode){
-        
-        status = new Status();
-        status = status.setStatus(unitcode);
-        this.state=State.awake;
-        return status;
-    }
-    public void enemyAtk(){
-        
-        playerHP-=status.atkDmg;
-        state=State.playerTurn;
-    }
-    public void enemyGetAtk(){
-        
-        
-        
-        status.nowHP-=playerAtk; 
-        if(status.nowHP<=0){Dead();}
-        
-
-    }
-    public void enemyGuard(){
-        if(playerAtk<status.atkDmg){
-                Debug.Log("Enemy방어 성공");
-                state=State.enemyTurn;
-            }else{status.nowHP-=playerAtk;state=State.enemyTurn;
-            Debug.Log("Enemy방어 실패");
-            if(status.nowHP<=0){Dead();}}
-
-    }
-    public void Dead(){
-        if(this.state==State.awake){
-            return;
-        }
-        Debug.Log("Enemy 패배,전투종료");
-
-        state=State.dead;
-
-    }
-    public void enemyTurn(){
-        if(this.state==State.enemyTurn){
-           return;
-            
-        }else{
-            this.state=State.enemyTurn;
-            random=Random.Range(0,9);
-            if(random<=5){
-                enemyGetAtk();
-                Debug.Log("Enemy공격");
-                enemyAtk();
-                }
-        else{Debug.Log("Enemy방어");
-            enemyGuard();}  
-        }
-            }
+    public int playerAtk=3;//각 턴마다 받아오는 player 공격량
+    
+    public int playerHP=10;//player의 현재 hp
+    public int playerDefendHP=0;//player의 별도 defendHP;
+    private int enemyDefendHP=0;//enemy가 방어action시 누적되는 별도 hp로 player공격시 가장 먼저 깎임
 
     
-    public void playerTurn(){
-        if(this.state==State.playerTurn){
-            return;
-        }else{
-            this.state=State.playerTurn;
-            enemyGetAtk();
-
-        }
-
-    }
+    
+    
     void Awake(){
-        status=setEnemy(unitcode);
+        
+        setEnemy(unitcode);//먼저 enemy 설정,state는 awake
+        
         
     }
     void Start()
-    {
-        if(playerSpeed>=status.Speed) state=State.playerTurn;
-        else state=State.enemyTurn;
+    {   
+        BattleStart();//배틀시작, 선제공격 정하고 state를 awake에서 변경
+        if(state==State.dead){
+            EndBattle();}//enemy가 dead일경우 배틀을 끝냄
         
     }
 
     // Update is called once per frame
     void Update()
     {
-            if(state!=State.dead){
-                Invoke("playerTurn",1f);
-            Invoke("enemyTurn",1f);
+        
+      }
+    void setEnemy(unitCode unitcode){
+        
+        this.status.setStatus(unitcode);
+        this.state=State.awake;
+        return;
+    }//unitcode를 받아와서 enemy설정, public변수로 유니티내에서 설정가능
+    void BattleStart(){
+        
+        if(playerSpeed>=status.Speed){
+            state=State.playerTurn;
+        }else{
+            enemyAction();
+        }
+    }//player, enemy중 더 speed가 높은 쪽에 먼저 턴이 돌아감(state변경)
+    
+
+    
+    void playerEndTurn(){
+        if(state!=State.playerTurn){//playerTurn이 아니면 버튼이 눌리지 않음
+            if(state==State.enemyTurn){
+                Debug.Log("EnemyTurn입니다.");}//enemyTurn이면 안내창
+        
+        return;
+        }
+        
+        enemyGetAtk();//player에게 받은 공격 합산
+        Invoke("enemyAction",1);//enemy 공격/방어
+
+    }//UI button에 연결할 함수. player의 턴종료, enemy턴 불러옴
+    void enemyAction(){
+       
+        if(state==State.dead){
+            EndBattle()};//enemy가 dead일경우 배틀을 끝냄
+        state=State.enemyTurn;
+
+        
+        int Dmg=random.Range(status.minAtkDmg,status.maxAtkDmg);//데미지 랜덤
+        if(random>2){
+               
+                Debug.Log("Enemy공격");
+                
+                
+                playerDefendHP-=Dmg;
+                playerHP+=playerDefendHP;
+                }//적공격: player에게 hp피해
+        else{Debug.Log("Enemy방어");
             }
+
+        state=State.playerTurn;
+    }
+    void enemyGetAtk(){
+        //n개의 카드에서 받아온 playerAtk을 여기서 합산-리스트나 배열로 받아와서 for문으로 합산
+        int Dfnd=random.Range(status.minAtkDmg,status.maxAtkDmg);//방어량 랜덤
+        random=Random.Range(0,9);
+        if(random<=2){
+            Debug.Log("Enemy방어");enemyDefendHP+=Dmg;}//30%확률로 방어,방어hp 추가
+
+
+        if(enemyDefendHP>0){
+            enemyDefendHP-=playerAtk;
+            if(enemyDefendHP<0)status.nowHP+=enemyDefendHP;}
+        else{status.nowHP-=playerAtk;}
             
+
+        
+        
+        if(status.nowHP<=0){Dead();} 
+        
+        
         
 
-    }
+    }//적 공격받음: player로부터 hp손실, hp=0이하일 경우 죽음
+    
+    void Dead(){
+        if(this.state==State.dead){
+            return;
+        }
+
+        state=State.dead;
+        EndBattle();
+
+    }//enemy 패배, 죽음
+    
+    
+    void EndBattle(){
+        Debug.Log("Enemy 패배, player승리");
+    }//enemy죽고 전투 승리(이후 씬 전환 코드 추가)
+
+    
+   
+    
 }
---
+++ enemyStats.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+이후 적 디자인, 밸런스 분배 필요*/
+public enum unitCode{
+    enemy1,
+    enemy2,
+    boss,
+}//각 적을 구분하는 코드.
+
+
+    public class enemyStats{
+        public unitCode unitcode{get;}//get:바꾸지 못하게 고정
+        public string name {get; set;}//enemy 이름
+        public int maxHP{get; set;}//최대 hp
+        public int nowHP{get; set;}//현재 hp
+        public int maxAtkDmg{get; set;}//공격력(방어력으로도 사용)최대
+        public int minAtkDmg{get; set;}//공격력(=방어력)최소
+        public int Speed{get;set;}//스피드(행동력)
+
+        public enemyStats(){
+
+        }
+        public enemyStats(unitCode unitcode, string name, int maxHP, int nowHP, int atkDmg, int Speed){
+            this.unitcode = unitcode;
+            this.name = name;
+            this.maxHP = maxHP;
+            this.nowHP = nowHP;
+            this.atkDmg = atkDmg;
+            this.Speed = Speed;
+        }
+
+
+
+       public enemyStats setStatus(unitCode unitcode){
+        enemyStats status = null;
+        switch(unitcode){
+            case unitCode.enemy1:
+                status = new enemyStats(unitcode,"적1",10, 10, 1,3, 5);
+                break;
+
+            case unitCode.enemy2:
+                status = new enemyStats(unitcode,"적2",20, 20, 4,7, 6);
+                break;
+
+            case unitCode.boss:
+                status = new enemyStats(unitcode,"보스",50, 50, 5,10, 2);
+                break;
+        }
+        return status;
+    }//unitcode에 따라 적을 지정하는 함수. 이후 적을 추가/제거하거나 스탯,이름 변경이 가능함 
+
+    }
+
+    
+    
+    
+   
+
